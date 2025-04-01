714bc246d052920f2d8231566c21bcd124a9c5b5
hugovettorello <hugovettorello13@gmail.com>
2025-04-01 06:49:33 -0300

    chore: add sentry libeary to the project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,27 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://8e18d12c7f75177fe5d24929d2f17feb@o4509077005795328.ingest.us.sentry.io/4509077013987328",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect
+    }),
+    Sentry.replayIntegration()
+  ],
+  tracesSampleRate: 1.0, 
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1, 
+  replaysOnErrorSampleRate: 1.0 
+});
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
